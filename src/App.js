2fa89b9b8c8b57ad435ba4f7a3d8592ce5f80a75
alicekalemba <alycekal@gmail.com>
2024-04-06 02:02:06 -0700

    added histogram

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const histogramData = [
+    { label: 'Alice', value: 100 },
+    { label: 'Bridget', value: 200 },
+    { label: 'Christo', value: 150 },
+    { label: 'Dave', value: 75 },
+    { label: 'Edrian', value: 50 }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ backgroundColor: 'gray', padding: '20px', marginBottom: '20px' }}>
+        <h1>Hello World!</h1>
+        <p>My name is Alice</p>
+        <img src="https://www.thesprucepets.com/thmb/Qt7k6w4eikK9ZDKULTEX7YEaAZs=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-181861505-4e63227ed0a14dc9bfe86848ef54caf3.jpg" alt="Cat" style={{ width: '500px' }} />
+      </div>
+
+      <div style={{ padding: '20px' }}>
+        <h2>The most performing clients</h2>
+        <div style={{ display: 'flex', gap: '10px', justifyContent: 'flex-start', alignItems: 'flex-end', height: '300px', border: '1px solid black', left: '10px', position: 'relative' }}>
+          {histogramData.map(data => (
+            <div key={data.label} style={{ width: '40px', height: `${data.value}px`, backgroundColor: 'blue', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center' }}>
+              <span style={{ color: 'white', margin: '5px 0' }}>{data.value}</span>
+            </div>
+          ))}
+        </div>
+        <div style={{marginTop: '5px' }}>
+          {histogramData.map(data => (
+            <span key={data.label} style={{ width: '40px', textAlign: 'center', marginRight: '10px' }}>{data.label}</span>
+          ))}
+        </div>
+        <p style={{ textAlign: 'left', marginTop: '10px' }}>Clients' Names</p>
+        <p style={{ position: 'absolute', top: '95%', left: '10px', transform: 'translateY(-50%) rotate(-90deg)' }}>Profits Made</p>
+      </div>
+    </>
   );
 }
 
